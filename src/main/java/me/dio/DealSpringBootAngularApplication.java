041cf0d8c5517ff5fe4fdcba1306b1ab8f9ac59b
gabrielszg <gabrielsouza5@hotmail.com>
2024-08-10 13:30:36 -0300

    first commit

---
+++ src/main/java/me/dio/DealSpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealSpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealSpringBootAngularApplication.class, args);
+	}
+
+}
